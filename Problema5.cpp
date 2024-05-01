4639411ff01d1ff18caaffd38260aa2feb9469d1
Javier <javierni.sanchez@gmail.com>
2024-05-01 14:40:38 +0200

    YOU TOUCH MY LALALLALA

--- Problema5.cpp
+++ Problema5.cpp
@@ -1,14 +1,50 @@
+#include <cstdlib>
 #include <iostream>
-#include <vector>
 using namespace std;
 
-#define N 4
+int N = 0;
 
-bool esValido(int laberinto[N][N], int x, int y) {
-    return (x >= 0 && x < N && y >= 0 && y < N && laberinto[x][y] == 1);
+void BorrarMatriz(int **matriz){
+  for(int i = 0; i < N; i++)
+    delete [] matriz[i];
+  delete[] matriz; 
 }
 
-bool resolverLaberintoUtil(int laberinto[N][N], int x, int y, int solucion[N][N]) {
+bool esValidoEscribir(int **laberinto, int x, int y) {
+  return (x >= 0 && x < N && y >= 0 && y < N && laberinto[x][y] == 0);
+}
+bool esValido(int **laberinto, int x, int y) {
+  return (x >= 0 && x < N && y >= 0 && y < N && laberinto[x][y] == 1);
+}
+
+void generarCasilla(int x, int y, int **laberinto){
+  if(esValido(laberinto, x, y)){
+    if(esValidoEscribir(laberinto, x-1, y)){
+      laberinto[x-1][y] = rand()%101 >= 50 ? 0: 1;
+      generarCasilla(x-1, y, laberinto);
+    }
+    if(esValidoEscribir(laberinto, x+1, y)){
+      laberinto[x+1][y] = rand()%101 >= 50 ? 0: 1;
+      generarCasilla(x+1, y, laberinto);
+    }
+    if(esValidoEscribir(laberinto, x, y-1)){
+      laberinto[x][y-1] = rand()%101 >= 50 ? 0: 1;
+      generarCasilla(x, y-1, laberinto);
+    }
+    else if(esValidoEscribir(laberinto, x, y+1)){
+      laberinto[x][y+1] = rand()%101 >= 50 ? 0: 1;
+      generarCasilla(x, y+1, laberinto);
+    }
+  }
+}
+void generarLaberinto(int **laberinto){
+  //tiene que haber una entrada y una salida
+  laberinto[0][0] = 1;
+  laberinto[N-1][N-1] = 1;
+  generarCasilla(0, 0, laberinto);
+}
+
+bool resolverLaberintoUtil(int **laberinto, int x, int y, int** solucion) {
     if(x == N-1 && y == N-1 && laberinto[x][y] == 1) {
         solucion[x][y] = 1;
         return true;
@@ -25,13 +61,16 @@ bool resolverLaberintoUtil(int laberinto[N][N], int x, int y, int solucion[N][N]
     return false;
 }
 
-bool resolverLaberinto(int laberinto[N][N]) {
-    int solucion[N][N] = { { 0, 0, 0, 0 },
-                           { 0, 0, 0, 0 },
-                           { 0, 0, 0, 0 },
-                           { 0, 0, 0, 0 } };
+bool resolverLaberinto(int** laberinto) {
+  int** solucion = new int*[N];
+  for(int i = 0; i < N; i++){
+    solucion[i] = new int[N];
+    for(int j = 0; j < N; j++)
+      solucion[i][j] = 0;
+  }
     if(resolverLaberintoUtil(laberinto, 0, 0, solucion) == false) {
-        cout << "No existe solución";
+        cout << "No existe solución" << endl;
+        BorrarMatriz(solucion);
         return false;
     }
     for (int i = 0; i < N; i++) {
@@ -39,14 +78,31 @@ bool resolverLaberinto(int laberinto[N][N]) {
             cout << solucion[i][j] << " ";
         cout << endl;
     }
+    
+    BorrarMatriz(solucion);
     return true;
 }
 
 int main() {
-    int laberinto[N][N] = { { 1, 0, 0, 0 },
-                            { 1, 1, 0, 1 },
-                            { 0, 1, 0, 0 },
-                            { 1, 1, 1, 1 } };
-    resolverLaberinto(laberinto);
+  while (N <= 4)
+    cin >> N;
+  int **laberinto;
+  laberinto = new int*[N];
+  for(int i = 0; i < N; i++){
+    laberinto[i] = new int[N];
+    for(int j = 0; j < N; j++)
+      laberinto[i][j] = 0;
+  }
+  generarLaberinto(laberinto);
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++)
+      cout << laberinto[i][j] << " ";
+    cout << endl;
+  }
+  cout << endl << endl;
+  resolverLaberinto(laberinto);
+  for(int i = 0; i < N; i++)
+    delete [] laberinto[i];
+  delete [] laberinto;
     return 0;
 }
